f141f16e501365e7839b61c4a106f1c1fad22c08
Nacho1899 <74553384+Nacho1899@users.noreply.github.com>
2020-11-16 14:45:39 -0600

    Update ResultWriter.java

--- src/io/ResultWriter.java
+++ src/io/ResultWriter.java
@@ -1,30 +1,30 @@
 package io;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
+import java.io.FileWriter; // Importamos la clase FileWriter
+import java.io.IOException; // Importamos la clase IOException
+import java.io.File; // Importamos la clase Archvo
 
 /**
  * ResultWriter
  */
 public class ResultWriter {
-    File file;
+    File file; // Creamos un archivo
 
     public ResultWriter(String file) {
-        this.file = new File(file);
+        this.file = new File(file); // Escribimos en el archivo un String
     }
 
     public void writeResults(int[] results) {
-        try {
-            FileWriter writer = new FileWriter(file);
+        try { // Inicio donde pudiera saltar un error 
+            FileWriter writer = new FileWriter(file);  // Creamos el archivo para escribir 
 
             for (int i : results) {
                 writer.write(String.valueOf(i) + "\n");
-                System.out.println(i);
+                System.out.println(i); // Leemos el archivo y lo mostramos por pantalla
             }
-            writer.close();
+            writer.close(); // Cerramos el arhivo 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Imprime donde fue el error 
         }
     }
 }
